35a3ec0bc9e40776fe3ae7fee638261ea2efa9d9
LadyAudrey <theaudreygreene@gmail.com>
2023-07-23 19:37:35 -0700

    adding header component and deleting tailwind

---
+++ app/components/TopHeader/page.tsx
@@ -0,0 +1,9 @@
+export  function TopHeader() {
+  return (
+    <>
+    <header>
+      Hi,I'm a header
+    </header>
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 // required for the routing to happen (other option is route.tsx)
 // I AM ROOT
+import { TopHeader } from "./components/TopHeader/page"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <TopHeader />
       <h1>Hi, I'm an H1 div</h1>
     </main>
   )
