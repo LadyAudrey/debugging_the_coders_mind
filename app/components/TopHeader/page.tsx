0467061da66bdf78780eedfed7ee654e06e63726
LadyAudrey <theaudreygreene@gmail.com>
2023-07-27 11:18:57 -0700

    reinstalling tailwind, add header figma code & supabase

--- app/components/TopHeader/page.tsx
+++ app/components/TopHeader/page.tsx
@@ -1,9 +1,13 @@
 export  function TopHeader() {
   return (
     <>
-    <header>
-      Hi,I'm a header
-    </header>
+    <div className="Topheader w-96 h-32 relative">
+      <div className="Rectangle3 w-96 h-32 left-0 top-0 absolute bg-lime-950" />
+      <div className="Ellipse3 w-28 h-28 left-[1796px] top-[12px] absolute bg-zinc-300 rounded-full border-2 border-yellow-950" />
+      <div className="Profile left-[1806px] top-[50px] absolute text-black text-4xl font-medium">Profile</div>
+      <div className="Rectangle6 w-96 h-14 left-[573px] top-[43px] absolute bg-zinc-300 rounded-3xl border-2 border-yellow-700" />
+      <div className="Search left-[1324px] top-[48px] absolute text-teal-900 text-4xl font-medium">search</div>
+      </div>
     </>
   )
 }
