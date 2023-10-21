945b511f309d1e310ec3cb46177aec49f1757a01
LadyAudrey <theaudreygreene@gmail.com>
2023-10-21 14:36:01 -0700

    debugging header issue

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col relative text-white">
-        {/* <Header /> */}
         <header className="absolute top-0 w-full h-full">
           <TopHeader />
           <TitleHeader />
